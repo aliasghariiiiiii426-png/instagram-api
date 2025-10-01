6b714c039a47d50e4dbb6cf06ffeb910eee4eaae
behnam <behnam.ahmadifar@gmail.com>
2025-10-01 16:51:34 +0330

    first commit

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def exchange():
 # ----------------------------
 @app.route("/profile")
 def profile():
-    token = session.get("long_lived_token")
+    token = 'IGAAJ2XaZAwPfVBZAFRCMFo4bUJ0R1Rzd0w0Ry1LcGFPQWFMcW9CVUJVdGZAjclVsUWNwakJlRUpXVlRSRTIza08xSldkVzktaThPSUE1UXJCbUgzY0FuTkJMd0lpSGxDeDIzb2tUUkpXNV9MVFgyZAjMwektGQ2p3U21qZAzlYLWR6TQZDZD'
     if not token:
         return "No token. Please login again.", 400
 
